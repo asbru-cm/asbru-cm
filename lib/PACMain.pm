fa1f8f9e9b00ec8b3489488311f0328ea87213f6
Hans Peyrot <hans@eflow.com.mx>
2019-10-24 10:08:58 -0500

    fix issue #245 pluss more fixes to names with <>

--- lib/PACMain.pm
+++ lib/PACMain.pm
@@ -1924,7 +1924,8 @@ sub _setupCallbacks {
         }
         # Favourites
         elsif ($page eq $$self{_GUI}{scroll2}) {
-            $self->_updateFavouritesList; $self->_updateGUIFavourites;
+            $self->_updateFavouritesList;
+            $self->_updateGUIFavourites;
         }
         # History
         elsif ($page eq $$self{_GUI}{scroll3}) {
@@ -1932,7 +1933,8 @@ sub _setupCallbacks {
         }
         # Clusters
         else {
-            $self->_updateClustersList; $self->_updateGUIClusters;
+            $self->_updateClustersList;
+            $self->_updateGUIClusters;
         }
         return 1;
     });
@@ -3075,7 +3077,7 @@ sub _launchTerminals {
         }
         my $uuid = $$t{_UUID};
         my $icon = $uuid eq '__PAC_SHELL__' ? Gtk3::Gdk::Pixbuf->new_from_file_at_scale($RES_DIR . '/asbru_shell.png', 16, 16, 0) : $$self{_METHODS}{ $$self{_CFG}{'environments'}{$uuid}{'method'} }{'icon'};
-        my $name = $$self{_CFG}{'environments'}{$uuid}{'name'};
+        my $name = __($$self{_CFG}{'environments'}{$uuid}{'name'});
         unshift(@{ $$self{_GUI}{treeHistory}{data} }, ({ value => [ $icon, $name, $uuid,  strftime("%H:%M:%S %d-%m-%Y", localtime($FUNCS{_STATS}{statistics}{$uuid}{start})) ] }));
     }
 
@@ -3622,7 +3624,9 @@ sub _updateGUIFavourites {
     $$self{_GUI}{connFavourite}->set_image(Gtk3::Image->new_from_stock('pac-favourite-on', 'button'));
     $$self{_NO_PROPAGATE_FAV_TOGGLE} = 0;
 
-    $self->_updateGUIWithUUID($sel_uuids[0]) if $total == 1;
+    if ($total == 1) {
+        $self->_updateGUIWithUUID($sel_uuids[0]);
+    }
 
     return 1;
 }
@@ -3697,10 +3701,10 @@ sub _updateClustersList {
 
     @{ $$self{_GUI}{treeClusters}{data} } = ();
     foreach my $ac (sort { $a cmp $b } keys %{ $$self{_CFG}{defaults}{'auto cluster'} }) {
-        push(@{ $$self{_GUI}{treeClusters}{data} }, ({ value => [ $AUTOCLUSTERICON, $ac ] }));
+        push(@{ $$self{_GUI}{treeClusters}{data} }, ({ value => [ $AUTOCLUSTERICON, $ac ]}));
     }
     foreach my $cluster (sort { $a cmp $b } keys %{ $$self{_CLUSTER}->getCFGClusters }) {
-        push(@{ $$self{_GUI}{treeClusters}{data} }, ({ value => [ $CLUSTERICON, $cluster ] }));
+        push(@{ $$self{_GUI}{treeClusters}{data} }, ({ value => [ $CLUSTERICON, $cluster ]}));
     }
 
     return 1;
@@ -3708,6 +3712,7 @@ sub _updateClustersList {
 
 sub _updateFavouritesList {
     my $self = shift;
+    my ($name);
 
     @{ $$self{_GUI}{treeFavourites}{data} } = ();
     foreach my $uuid (keys %{ $$self{_CFG}{'environments'} }) {
@@ -3715,7 +3720,14 @@ sub _updateFavouritesList {
             next;
         }
         my $icon = $$self{_METHODS}{ $$self{_CFG}{'environments'}{$uuid}{'method'} }{'icon'};
-        my $name = $$self{_CFG}{'environments'}{$uuid}{'name'};
+        my $group = $$self{_CFG}{'environments'}{$uuid}{'parent'};
+        if ($group) {
+            $name = __($$self{_CFG}{'environments'}{$uuid}{'name'});
+            $group = __("$$self{_CFG}{'environments'}{$group}{'name'} : ");
+            $name = "$group$name";
+        } else {
+            $name = __($$self{_CFG}{'environments'}{$uuid}{'name'});
+        }
         push(@{ $$self{_GUI}{treeFavourites}{data} }, ({ value => [ $icon, $name, $uuid ] }));
     }
 
