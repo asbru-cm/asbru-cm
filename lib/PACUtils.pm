42741fccdab90b02a7c8fbac9cc2d9ae778bf01f
Hans Peyrot <hans@eflow.com.mx>
2019-10-25 07:20:01 -0500

    fix issue #132

--- lib/PACUtils.pm
+++ lib/PACUtils.pm
@@ -3347,6 +3347,8 @@ sub _removeEscapeSeqs {
     $string =~ s/\e\[[0-9;]*m(?:\e\[K)?//g;
     $string =~ s/\x1B.+~\x07//g;
     $string =~ s/(\x1B|\x08|\x07)(\[w)?//g;
+    $string =~ s/\[\?\d+h//g;
+    $string =~ s/\]\d;//g;
 
     return $string;
 }
