122194f7c40897811e0a6548cb92e2a680024e7e
Hans Peyrot <hans@eflow.com.mx>
2019-11-06 22:25:22 -0600

    fixes to layout on different desktops 3

--- lib/PACMain.pm
+++ lib/PACMain.pm
@@ -2715,6 +2715,17 @@ sub _treeConnections_menu {
         sensitive =>  (scalar(@sel) == 1) && ($$self{_CFG}{'environments'}{$sel[0]}{'_is_group'} || $sel[0] eq '__PAC__ROOT__'),
         code => sub { $self->__importNodes }
     });
+    if ($ENV{'ASBRU_DESKTOP'} eq 'gnome-shell') {
+        # Display settings options in gnome-shell, there is no tray icon to access it
+        push(@tree_menu_items, {
+            label => 'Settings...',
+            stockicon => 'gtk-preferences',
+            shortcut => '',
+            tooltip => 'Settings',
+            sensitive =>  1,
+            code => sub { $$self{_GUI}{configBtn}->clicked; }
+        });
+    }
     # Quick Edit variables
     my @var_submenu;
     my $i = 0;
