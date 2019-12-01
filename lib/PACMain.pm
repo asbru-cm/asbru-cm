dfd8e5d9bf7ab8a9d806b6f0714529a384cba4d2
Hans Peyrot <hans@eflow.com.mx>
2019-11-30 21:49:41 -0600

    removed code from utf8icons experiment, caused conflicts with bold markup

--- lib/PACMain.pm
+++ lib/PACMain.pm
@@ -534,7 +534,7 @@ sub _initGUI {
     $$self{_GUI}{treeConnections}->set_enable_search(0);
     $$self{_GUI}{treeConnections}->set_has_tooltip(1);
     $$self{_GUI}{treeConnections}->set_grid_lines('GTK_TREE_VIEW_GRID_LINES_NONE');
-    $$self{_GUI}{treeConnections}->set_level_indentation(-2);
+#    $$self{_GUI}{treeConnections}->set_level_indentation(-2);
     # Implement a "TreeModelSort" to auto-sort the data
     my $sort_model_conn = Gtk3::TreeModelSort->new_with_model($$self{_GUI}{treeConnections}->get_model);
     $$self{_GUI}{treeConnections}->set_model($sort_model_conn);
@@ -910,9 +910,7 @@ sub _initGUI {
     # Set treeviews font
     foreach my $tree ('Connections', 'Favourites', 'History') {
         my @col = $$self{_GUI}{'tree' . $tree}->get_columns;
-        my ($c) = $col[1]->get_cells;
-        $c->set('font', $$self{_CFG}{defaults}{'tree font'});
-        ($c) = $col[0]->get_cells;
+        my ($c) = $col[0]->get_cells;
         $c->set_alignment(1,0.5);
     }
 
