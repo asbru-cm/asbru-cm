35b3538226ed9974a7bc746319cf9a6a719fa007
Tammling, Klaas <klaas.tammling@akquinet.de>
2020-02-04 09:15:26 +0100

    Bumping and changelog

--- lib/PACUtils.pm
+++ lib/PACUtils.pm
@@ -119,7 +119,7 @@ require Exporter;
 # Define GLOBAL CLASS variables
 
 our $APPNAME = 'Ásbrú Connection Manager';
-our $APPVERSION = '6.0.4';
+our $APPVERSION = '6.0.5';
 our $DEBUG_LEVEL = 1;
 our $ARCH = '';
 my $ARCH_TMP = `/bin/uname -m 2>&1`;
