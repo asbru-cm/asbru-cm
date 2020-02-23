6595d50260b2bedffc2f2cffe6f6f6a0990907a8
Hans Peyrot <hans@eflow.com.mx>
2020-02-23 12:13:57 -0600

    removed old keepass code

--- lib/PACUtils.pm
+++ lib/PACUtils.pm
@@ -1478,11 +1478,12 @@ sub _wEnterValue {
     }
 
     my %w;
-
     if (defined $WINDOWSPLASH{_GUI}) {
         $parent = $WINDOWSPLASH{_GUI};
     } elsif (undef $parent) {
-        $parent = $PACMain::FUNCS{_MAIN}{_GUI}{main};
+        if (defined $PACMain::FUNCS{_MAIN}{_GUI}{main}) {
+            $parent = $PACMain::FUNCS{_MAIN}{_GUI}{main};
+        }
     }
     # Create the dialog window,
     $w{window}{data} = Gtk3::Dialog->new_with_buttons(
