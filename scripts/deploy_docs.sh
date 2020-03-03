938d19a30bf4c13c8deef7e910abf51dfdc39fec
Klaas Tammling <klaas@tammling.hamburg>
2020-03-03 22:41:15 +0100

    First deploy script for docs. Not linked to the travis job yet.

---
+++ scripts/deploy_docs.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+GH_REPO="@github.com/asbru-cm-docs/asbrucm-docs.github.io.git"
+FULL_REPO="https://${GH_TOKEN}$GH_REPO"
+
+if [ "$EXECUTE_BUILD_DOCS" != "true" ]; then
+    echo "Doc build skipped"
+    exit 0
+fi
+
+pip3 install --upgrade pip
+pip3 install --user --requirement <(cat <<EOF
+Click==7.0
+future==0.18.2
+Jinja2==2.11.1
+livereload==2.6.1
+lunr==0.5.6
+Markdown==3.2.1
+MarkupSafe==1.1.1
+mkdocs==1.1
+mkdocs-material==4.6.3
+nltk==3.4.5
+Pygments==2.5.2
+pymdown-extensions==6.3
+PyYAML==5.3
+six==1.14.0
+tornado==6.0.3
+EOF
+)
+
+
+mkdir -p out
+cd out
+
+git init
+git remote add origin "$FULL_REPO"
+git fetch
+git config user.name "asbrucm-docs"
+git config user.email "travis@asbru-cm.net"
+git checkout master
+
+cd ../
+mkdocs build --clean
+build_result=$?
+
+# Only deploy after merging to master
+if [ "$build_result" == "0" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    cd out/
+    touch .
+    git add -A .
+    git commit -m "GH-Pages update by travis after $TRAVIS_COMMIT"
+    git push -q origin master
+else
+    exit ${build_result}  # return doc build result
+fi
