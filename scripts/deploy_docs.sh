85bb72de6cfdca5817412bc85b71414bfb406d10
Klaas Tammling <klaas@tammling.hamburg>
2020-04-26 10:46:13 +0200

    Another change in dependencies for python 3.6

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -28,6 +28,26 @@ gem install rack -v 1.6.4
 gem install github_changelog_generator
 github_changelog_generator --token ${GITHUB_API_KEY} --release-branch master --user asbru-cm --project asbru-cm --output doc/General/Changelog.md
 
+pip3 install --upgrade pip
+pip3 install --user --requirement <(cat <<EOF
+Click==7.0
+future==0.18.2
+Jinja2==2.11.1
+livereload==2.6.1
+lunr==0.5.6
+Markdown==3.2.1
+MarkupSafe==1.1.1
+mkdocs==1.1
+mkdocs-material==4.6.3
+nltk==3.4.5
+Pygments==2.5.2
+pymdown-extensions==6.3
+PyYAML==5.3
+six==1.14.0
+tornado==6.0.3
+EOF
+)
+
 mkdocs build --clean
 	
 build_result=$?
