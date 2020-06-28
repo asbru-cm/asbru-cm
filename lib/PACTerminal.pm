cf9c5ab03a43b520c09e3f94834d55b7291b8a4f
Gaëtan Frenoy <gaetan@frenoy.net>
2020-06-28 17:27:55 +0200

    :bug: Fix copy/paste of cyrillic text (#652)

--- lib/PACTerminal.pm
+++ lib/PACTerminal.pm
@@ -1179,7 +1179,7 @@ sub _setupCallbacks {
         elsif ($shift && (! $$self{_CFG}{environments}{$$self{_UUID}}{'terminal options'}{'disable SHIFT key bindings'})) {
             # <ins> --> PASTE
             if ($keyval eq 'Insert') {
-                $$self{_GUI}{_VTE}->paste_clipboard;
+                $$self{_GUI}{_VTE}->paste_primary();
                 return 1;
             }
         }
@@ -2344,7 +2344,7 @@ sub _pasteToVte {
             select(undef, undef, undef, $slow / 1000);
         }
     } else {
-        $$self{_GUI}{_VTE}->paste_clipboard;
+        $$self{_GUI}{_VTE}->paste_primary();
     }
 }
 
@@ -4427,7 +4427,7 @@ Display the popup Terminal menu on [shift] - right click
 Takes information from the clipboard and sends it to the terminal
 
     if slow : Use _vteFeedChild
-    else {_GUI}{_VTE}->paste_clipboard
+    else {_GUI}{_VTE}->paste_primary
 
 with _vteFeedChild
 
