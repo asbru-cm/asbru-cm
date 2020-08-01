987d312e8d115c72d7d203ae09d65eb5e38b71b2
Syco <alberto.rinaudo+dino@gmail.com>
2020-08-01 18:50:50 +0100

    test new copy and paste
    Signed-off-by: Syco <alberto.rinaudo+dino@gmail.com>

--- lib/PACKeyBindings.pm
+++ lib/PACKeyBindings.pm
@@ -415,8 +415,10 @@ sub _getDefaultConfig {
     $$cfg{'terminal'}{'AltCtrl+x'}     = ['Terminal','reset','Reset Terminal'];
     $$cfg{'terminal'}{'AltCtrl+X'}     = ['Terminal','reset-clear','Reset Terminal and Clear window'];
     $$cfg{'terminal'}{'CtrlAlt+r'}     = ['Terminal','remove_from_cluster','Remove terminal from cluster'];
+    $$cfg{'terminal'}{'Ctrl+C'}        = ['Terminal','copy','Copy selection to clipboard'];
+    $$cfg{'terminal'}{'Ctrl+V'}        = ['Terminal','paste','Paste clipboard into terminal'];
     $$cfg{'terminal'}{'Ctrl+Insert'}   = ['Terminal','copy','Copy selection to clipboard'];
-    $$cfg{'terminal'}{'Shift+Insert'}  = ['Terminal','paste','Paste selection into terminal'];
+    $$cfg{'terminal'}{'Shift+Insert'}  = ['Terminal','paste-primary','Paste selection into terminal'];
     $$cfg{'terminal'}{'Ctrl+p'}        = ['Terminal','paste-passwd','Paste terminal password'];
     $$cfg{'terminal'}{'Ctrl+b'}        = ['Terminal','paste-delete','Paste and regex delete'];
     $$cfg{'terminal'}{'Ctrl+g'}        = ['Terminal','hostname','Guess hostname'];
