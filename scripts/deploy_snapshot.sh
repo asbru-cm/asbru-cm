b33f70341364c2097d1ae5fa446a95c6b9d47ebf
Tammling, Klaas <klaas.tammling@akquinet.de>
2020-08-06 15:11:56 +0200

    Check for source files and metadata on Debian based packages

--- scripts/deploy_snapshot.sh
+++ scripts/deploy_snapshot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-for f in build/*.{deb,rpm,dsc}
+for f in build/*.${PACKAGE}
 do
 	echo "Processing $f"
         cloudsmith push ${PACKAGE} asbru-cm/asbru-cm/${OS}/${DIST} $f
 done
+
+if [ $PACKAGE == "deb" ]
+then
+	cloudsmith push ${PACKAGE} asbru-cm/asbru-cm/${OS}/${DIST} build/*.dsc --sources-file build/*.tar.xz
+fi
