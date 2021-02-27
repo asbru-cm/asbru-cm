076b790da5a7bbf1e8856666cb58af6ae038d2a7
Gaëtan Frenoy <gaetan@frenoy.net>
2021-02-27 12:52:11 +0100

    :bookmark: Bump to version 6.3.3

--- lib/PACUtils.pm
+++ lib/PACUtils.pm
@@ -121,7 +121,7 @@ require Exporter;
 # Define GLOBAL CLASS variables
 
 our $APPNAME = 'Ásbrú Connection Manager';
-our $APPVERSION = '6.3.2';
+our $APPVERSION = '6.3.3';
 our $DEBUG_LEVEL = 1;
 our $ARCH = '';
 my $ARCH_TMP = `/bin/uname -m 2>&1`;
