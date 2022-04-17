f3bdca66dd7d01b32c1111c2d345f9747a484bac
Gaëtan Frenoy <gaetan@frenoy.net>
2022-04-17 15:37:56 +0200

    :art: Replace tab with spaces

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 if [ -z "$TRAVIS_TAG" ]; then
-	eval "$(egrep -o 'APPVERSION.*=.*' lib/PACUtils.pm | tr -d '[:space:]')"
-	export VERSION=$APPVERSION~$(date +"%s");
-	echo "No Travis Tag set. We are using a timestamp in seconds: ${VERSION}"
+  eval "$(egrep -o 'APPVERSION.*=.*' lib/PACUtils.pm | tr -d '[:space:]')"
+  export VERSION=$APPVERSION~$(date +"%s");
+  echo "No Travis Tag set. We are using a timestamp in seconds: ${VERSION}"
 else
-	export VERSION=$TRAVIS_TAG
-	echo "Our version will be the tag ${VERSION}"
+  export VERSION=$TRAVIS_TAG
+  echo "Our version will be the tag ${VERSION}"
 fi
 
 cp -r dist/${PACKAGE}/* .
 
 if [ "${SCRIPT}" == "make_debian.sh" ]; then
-	mkdir build
-	./make_debian.sh
-	cp *.{deb,tar.xz,dsc,build,changes} build/
+  mkdir build
+  ./make_debian.sh
+  cp *.{deb,tar.xz,dsc,build,changes} build/
 else
-	git clone https://github.com/packpack/packpack.git packpack
-	./packpack/packpack
+  git clone https://github.com/packpack/packpack.git packpack
+  ./packpack/packpack
 fi
