asbru-cm/asbru-cm

commit 2a4cb8731eba1b1dad34f8ab386b20ed2cfce577

:children_crossing: Review the behavior of the show/hide button bar (#954)

lib/PACTerminal.pm
@@ -817,12 +817,10 @@ sub _initGUI {
     # Only in traditional mode, show additional buttons
     if ($$self{_CFG}{'defaults'}{'layout'} ne 'Compact') {
         # Create a checkbox to show/hide the button bar
-        $$self{_GUI}{btnShowButtonBar} = Gtk3::ToggleButton->new();
+        $$self{_GUI}{btnShowButtonBar} = Gtk3::Button->new();
         $$self{_GUI}{btnShowButtonBar}->set_image(Gtk3::Image->new_from_stock($$self{_CFG}{'defaults'}{'auto hide button bar'} ? 'asbru-buttonbar-show' : 'asbru-buttonbar-hide', 'GTK_ICON_SIZE_BUTTON'));
         $$self{_GUI}{btnShowButtonBar}->set('can-focus' => 0);
         $$self{_GUI}{btnShowButtonBar}->set_tooltip_text('Show/Hide button bar');
-        $$self{_GUI}{btnShowButtonBar}->set_active($$self{_CFG}{'defaults'}{'auto hide button bar'} ? 0 : 1);
-        $$self{_GUI}{btnShowButtonBar}->set_inconsistent(0);
         $$self{_GUI}{bottombox}->pack_end($$self{_GUI}{btnShowButtonBar}, 0, 1, 4);
 
         # Create a button to show the info tab
@@ -990,13 +988,15 @@ sub _setupCallbacks {
 
     # Show Bar (if any)
     if ($$self{_GUI}{btnShowButtonBar}) {
-        $$self{_GUI}{btnShowButtonBar}->signal_connect('toggled', sub {
-            if ($$self{_GUI}{btnShowButtonBar}->get_active()) {
+        $$self{_GUI}{btnShowButtonBar}->signal_connect('clicked', sub {
+            if ($$self{'_CFG'}{'defaults'}{'auto hide button bar'}) {
                 $$self{_GUI}{btnShowButtonBar}->set_image(Gtk3::Image->new_from_stock('asbru-buttonbar-hide', 'GTK_ICON_SIZE_BUTTON'));
                 $PACMain::FUNCS{_MAIN}{_GUI}{hbuttonbox1}->show();
+                $$self{'_CFG'}{'defaults'}{'auto hide button bar'} = 0;
             } else {
                 $$self{_GUI}{btnShowButtonBar}->set_image(Gtk3::Image->new_from_stock('asbru-buttonbar-show', 'GTK_ICON_SIZE_BUTTON'));
                 $PACMain::FUNCS{_MAIN}{_GUI}{hbuttonbox1}->hide();
+                $$self{'_CFG'}{'defaults'}{'auto hide button bar'} = 1;
             };
         });
     }
@@ -2480,6 +2480,10 @@ sub _setTabColour {
     } else {
         defined $$self{_WINDOWTERMINAL} and $$self{_WINDOWTERMINAL}->set_icon_from_file($$self{CONNECTED} ? "$RealBin/res/asbru_terminal64x64.png" : "$RealBin/res/asbru_terminal_x64x64.png");
     }
+
+    # Set correct icon for hide/show button bar button
+    # (it may have changed in another terminal)
+    $$self{_GUI}{btnShowButtonBar}->set_image(Gtk3::Image->new_from_stock($$self{_CFG}{'defaults'}{'auto hide button bar'} ? 'asbru-buttonbar-show' : 'asbru-buttonbar-hide', 'GTK_ICON_SIZE_BUTTON'));
 
     # Once checked the availability of new data, reset its value
     $$self{_NEW_DATA} = 0;

lib/PACMain.pm
@@ -2296,7 +2296,10 @@ sub _setupCallbacks {
 
         $self->_doFocusPage($pnum);
 
-        $$self{_GUI}{hbuttonbox1}->set_visible(($pnum == 0) || ($pnum && ! $$self{'_CFG'}{'defaults'}{'auto hide button bar'}));
+        # Show/hide the button bar
+        $$self{_GUI}{hbuttonbox1}->set_visible($pnum == 0 || !$$self{'_CFG'}{'defaults'}{'auto hide button bar'});
+
+        # Show/hide the connections list
         if (($pnum == 0)&&($$self{_CFG}{'defaults'}{'auto hide connections list'})) {
             # Info Tab, show connection list
             $$self{_GUI}{showConnBtn}->set_active(1);

commit c4a25d1e68f9546cbe6ff10621b6a168c2afb28b

:bug: Fix issue introduced with #954 when in compact mode (#1103)

lib/PACTerminal.pm
@@ -2482,7 +2482,9 @@ sub _setTabColour {
 
     # Set correct icon for hide/show button bar button
     # (it may have changed in another terminal)
-    $$self{_GUI}{btnShowButtonBar}->set_image(Gtk3::Image->new_from_stock($$self{_CFG}{'defaults'}{'auto hide button bar'} ? 'asbru-buttonbar-show' : 'asbru-buttonbar-hide', 'GTK_ICON_SIZE_BUTTON'));
+    if ($$self{_GUI}{btnShowButtonBar}) {
+        $$self{_GUI}{btnShowButtonBar}->set_image(Gtk3::Image->new_from_stock($$self{_CFG}{'defaults'}{'auto hide button bar'} ? 'asbru-buttonbar-show' : 'asbru-buttonbar-hide', 'GTK_ICON_SIZE_BUTTON'));
+    }
 
     # Once checked the availability of new data, reset its value
     $$self{_NEW_DATA} = 0;